5438a227b1e42c581c29ef9f5feb0dafe7d6e6d9
halfields <hfields697@aol.com>
2016-03-06 22:14:42 +0000

    nitrous-backup at Sun Mar  6 22:14:42 UTC 2016

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+insert into bears (name,age,gender,color,temperment,alive) values ("Mr. Chocolate",6,"male","tan","docile",1);
+insert into bears (name,age,gender,color,temperment,alive) values ("Rowdy",12,"male","black","intense",1);
+insert into bears (name,age,gender,color,temperment,alive) values ("Tabitha",16,"female","blue-gray","docile",1);
+insert into bears (name,age,gender,color,temperment,alive) values ("Sergeant Brown",20,"male","brown","calm",0);
+insert into bears (name,age,gender,color,temperment,alive) values ("Melissa",9,"female","white","nice"1);
+insert into bears (name,age,gender,color,temperment,alive) values ("Grinch",18,"male","cinnamon","agressive",1);
+insert into bears (name,age,gender,color,temperment,alive) values ("Wendy",14,"female","brown","naive",1);
+insert into bears (name,age,gender,color,temperment,alive) values (null,20,"male","brown","agressive",0);
